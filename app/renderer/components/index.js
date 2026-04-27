5b99ccab802329d6d685c78be56e29ad78a6c485
VPNUK <danny@vpnuk.info>
2026-04-27 20:30:36 +0100

    v1.3.0 update

---
+++ app/renderer/components/index.js
@@ -0,0 +1,20 @@
+// common
+export { default as ValueSelector } from './common/ValueSelector';
+
+// connection
+export { default as ConnectionDetails } from './connection/ConnectionDetails';
+export { default as ConnectionButton } from './connection/ConnectionButton';
+export { default as ConnectionSwitch } from './connection/ConnectionSwitch';
+
+// all
+export { default as ConfigEditor } from './ConfigEditor';
+export { default as LoadingGate } from './common/LoadingGate';
+export { default as MainPage } from './MainPage';
+export { default as Menu } from './Menu';
+export { default as OvpnDetails } from './OvpnDetails';
+export { default as ProfileDetails } from './ProfileDetails';
+export { default as ServerSelector } from './ServerSelector';
+export { default as Sidebar } from './Sidebar';
+export { default as Starting } from './Starting';
+export { default as UpdateInfo } from './UpdateInfo';
+export { default as WireGuardDetails } from './WireGuardDetails';
