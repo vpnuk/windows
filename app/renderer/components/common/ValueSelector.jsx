8b5e03f3b38502a17fc4ba00e46831341f7c9236
VPNUK <danny@vpnuk.info>
2026-04-27 20:30:30 +0100

    v1.3.0 update

---
+++ app/renderer/components/common/ValueSelector.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import Select from 'react-select';
+import { selectOptionColors } from '@styles';
+
+const ValueSelector = ({ options, onChange, defaultValue = undefined, value = undefined, formatOptionLabel = undefined }) =>
+    <Select
+        className="form-select"
+        styles={selectOptionColors}
+        options={options}
+        value={value}
+        defaultValue={defaultValue}
+        getOptionLabel={option => option.label}
+        formatOptionLabel={formatOptionLabel}
+        onChange={value => onChange(value)} />;
+
+export default observer(ValueSelector);
